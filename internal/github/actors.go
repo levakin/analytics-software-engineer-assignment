5c88dda400e42b7f3b6ccbd72d6edb384d54d5e0
a.levakin <a.levakin@napoleonit.ru>
2021-08-11 00:10:41 +0500

    add primitive functions to count actor activity

--- internal/github/actors.go
+++ internal/github/actors.go
@@ -10,3 +10,41 @@ type Actor struct {
 func TopNActiveActors(n int, actors []Actor, commits []Commit, events []Event) ([]Actor, error) {
 	return actors[0:n], nil
 }
+
+func calculateActorActivity(actorID string, commits []Commit, events []Event) int {
+	return countActorsPushedCommits(actorID, commits, events) + countActorsCreatedPullRequests(actorID, events)
+}
+
+func countActorsPushedCommits(actorID string, commits []Commit, events []Event) int {
+	var count int
+
+	for _, e := range events {
+		if e.ActorID != actorID || e.Type != pushEventType {
+			continue
+		}
+
+		for _, c := range commits {
+			if c.EventID != e.ID {
+				continue
+			}
+
+			count++
+		}
+	}
+
+	return count
+}
+
+func countActorsCreatedPullRequests(actorID string, events []Event) int {
+	var count int
+
+	for _, e := range events {
+		if e.ActorID != actorID || e.Type != pullRequestEventType {
+			continue
+		}
+
+		count++
+	}
+
+	return count
+}
--- internal/github/events.go
+++ internal/github/events.go
@@ -1,7 +1,12 @@
 package github
 
+const (
+	pullRequestEventType = "PullRequestEvent"
+	pushEventType        = "PushEvent"
+)
+
 type Event struct {
-	ID      int    `csv:"id"`
+	ID      string `csv:"id"`
 	Type    string `csv:"type"`
 	ActorID string `csv:"actor_id"`
 	RepoID  string `csv:"repo_id"`
